0d52d6ff50f077d8c84bed841ca4e9b3e4a1dde0
EkaterinaNovv <ekaterinabuyanovich@yandex.ru>
2025-11-11 16:41:28 +0700

    Update population_data.py
    Добавлен конструктор __init__ для загрузки CSV и расчета прироста

--- population_data.py
+++ population_data.py
@@ -1,5 +1,7 @@
-import pandas as pd
-
 class PopulationData:
     """Класс для хранения и обработки данных о населении"""
-    pass
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.data = pd.read_csv(filepath)
+        self.data['Прирост, %'] = self.data['Население'].pct_change() * 100
