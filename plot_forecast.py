a141e92eb7a5579483fdb18ff1bf3b22f93703a8
irasap05 <55658518+irasap05@users.noreply.github.com>
2025-11-11 16:43:21 +0700

    Create plot_forecast.py
    Создан класс PopulationAppWithPlot и добавлены необходимые импорты

---
+++ plot_forecast.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from gui_app import PopulationApp  # импорт класса интерфейса
+
+class PopulationAppWithPlot(PopulationApp):
+    """Добавление графиков и прогноза к основному интерфейсу"""
+    pass
